fffe1f42990907c1fca073744b6d34f41e17983c
BraumAce <1693717911@qq.com>
2024-08-08 21:46:59 +0800

    用户常量

---
+++ user-center-backend/src/main/java/com/yuan/constant/UserConstant.java
@@ -0,0 +1,17 @@
+package com.yuan.constant;
+
+/**
+ * 用户常量
+ * @author BraumAce
+ */
+public interface UserConstant {
+
+    // 用户登录态键
+    String USER_LOGIN_STATE = "登录成功";
+
+    // 默认权限
+    int DEFAULT_ROLE = 0;
+
+    // 管理员权限
+    int ADMIN_ROLE = 1;
+}
