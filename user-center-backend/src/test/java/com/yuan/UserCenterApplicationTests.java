0bd0f4eef2e9db5cf374b84a7c44da7920a71c53
BraumAce <1693717911@qq.com>
2024-08-17 23:48:14 +0800

    修改测试类

--- user-center-backend/src/test/java/com/yuan/UserCenterApplicationTests.java
+++ user-center-backend/src/test/java/com/yuan/UserCenterApplicationTests.java
@@ -40,46 +40,56 @@ public class UserCenterApplicationTests {
     @Test
     void testUserRegister() {
         // 测试非空
-        String usernName = "zhangsan";
-        String passward = "";
+        String userName = "zhangsan";
+        String password = "";
         String checkPassword = "12345678";
-        long result = userService.userRegister(usernName, passward, checkPassword);
+        long result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 测试账户长度小于4
-        usernName = "zhang";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        userName = "zhang";
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 测试密码小于6位
-        usernName = "zhangsan";
-        passward = "1234";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        userName = "zhangsan";
+        password = "1234";
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 测试特殊字符
-        usernName = "zhangsan@";
-        passward = "12345678";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        userName = "zhangsan@";
+        password = "12345678";
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 测试密码和校验密码不相同
-        usernName = "zhangsan";
+        userName = "zhangsan";
         checkPassword = "123457899";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 测试账号不重复
-        usernName = "zhangsan";
+        userName = "zhangsan";
         checkPassword = "12345678";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
 
         // 插入数据
-        usernName = "zhangsan";
-        passward = "123456789";
-        checkPassword = "123456789";
-        result = userService.userRegister(usernName, passward, checkPassword);
+        userName = "zhangsan";
+        password = "12345678";
+        checkPassword = "12345678";
+        result = userService.userRegister(userName, password, checkPassword);
         Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    void testAddUser() {
+        // 测试注册新用户
+        String userName = "lisi";
+        String password = "12345678";
+        String checkPassword = "12345678";
+        long result = userService.userRegister(userName, password, checkPassword);
+        System.out.println("新增用户ID：" + result);
     }
 }
